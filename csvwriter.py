4fb9fdfe9b79263ad4e7c97a7fd4680dbe71ceff
JAY <stanleyjs@hendrix.edu>
2016-12-22 04:59:03 -0600

    fixed file appending bug

--- csvwriter.py
+++ csvwriter.py
@@ -24,31 +24,31 @@ if __name__=="__main__":
 
 	outcsv = args.ofolder[0]+'/jobs.csv'
 	jobsfile = args.ofolder[0] + '/jobs.list'
-	# with open(outcsv,'w') as outputfile:
-	# 	for f in files:
-	# 		file = open(f, 'r')
-	# 		lines = [line for line in file.read().splitlines() if line]
-	# 		file.close()
+	with open(outcsv,'a') as outputfile:
+		for f in files:
+			file = open(f, 'r')
+			lines = [line for line in file.read().splitlines() if line]
+			file.close()
 
-	# 		genename = file.name.split('/')[-1].split('_')[0]
-	# 		try:
-	# 			poly = next(filter(lambda s: s.startswith('Polymorphism:'),lines)).split()[1]
-	# 			div = next(filter(lambda s: s.startswith('Divergence:'),lines)).split()[1]
-	# 			clength = len(poly)
-	# 			polyfile = args.ofolder[0] + '/' + genename + '_poly_consensus.txt'
-	# 			divfile = args.ofolder[0] + '/' + genename + '_div_consensus.txt'
-	# 			fwlen.append((clength, genename, polyfile, divfile))
-	# 			with open(polyfile,'w') as file:
-	# 				file.write('>Polymorphism \n' + poly)
-	# 			with open(divfile,'w') as file:
-	# 				file.write('>Divergence \n' + div)
-	# 		except:
-	# 			print(genename)
-	# 	fwlen.sort()
-	# 	string = '';
-	# 	for t in fwlen:
-	# 		string = string + t[1] +', ' + str(t[0]) + ', ' +  t[2] + ', ' + t[3] + ", source ~/.bashrc; source ~/.bash_profile; cd ../massprfout; massprf -p  " + t[2] + ' -d ' + t[3] + ' -ic 1 -sn 49 -ci_m 1 -r 1 -ci_r 1 -exact 0 -mn 10000 -t 1 -v 0 >> ' + t[1] + '.prfout \n'
-	# 		outputfile.write(string)
+			genename = file.name.split('/')[-1].split('_')[0]
+			try:
+				poly = next(filter(lambda s: s.startswith('Polymorphism:'),lines)).split()[1]
+				div = next(filter(lambda s: s.startswith('Divergence:'),lines)).split()[1]
+				clength = len(poly)
+				polyfile = args.ofolder[0] + '/' + genename + '_poly_consensus.txt'
+				divfile = args.ofolder[0] + '/' + genename + '_div_consensus.txt'
+				fwlen.append((clength, genename, polyfile, divfile))
+				with open(polyfile,'w') as file:
+					file.write('>Polymorphism \n' + poly)
+				with open(divfile,'w') as file:
+					file.write('>Divergence \n' + div)
+			except:
+				print(genename)
+		fwlen.sort()
+		string = '';
+		for t in fwlen:
+			string = t[1] +', ' + str(t[0]) + ', ' +  t[2] + ', ' + t[3] + ", source ~/.bashrc; source ~/.bash_profile; cd ../massprfout; massprf -p  " + t[2] + ' -d ' + t[3] + ' -ic 1 -sn 49 -ci_m 1 -r 1 -ci_r 1 -exact 0 -mn 10000 -t 1 -v 0 >> ' + t[1] + '.prfout \n'
+			outputfile.write(string)
 	with open(jobsfile, 'a') as jobsfile:
 		parsefile = open(outcsv, 'r')
 		print(parsefile.name)
