292fdb3b9e866349155bf81f15fca5324becedf2
JAY <stanleyjs@hendrix.edu>
2017-02-21 12:51:01 -0500

    removed excess files

--- macprejobs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-cd /ysm-gpfs/home/jss245/scratch/20161215aligntrim/ATout;
-for f in Pol_*.txt
-do
-	div=`echo "Div_${f:4:9}"*`
-	echo "source ~/.bashrc; cd /ysm-gpfs/home/jss245/scratch/20161215aligntrim/ATout; MASSPRF_preprocess -p $f -d $div -o 1 -ci_m 1 -s 1 -t 1 > /ysm-gpfs/home/jss245/scratch/20161215consensus/${f:4:9}_MACPRF_prep.txt" >> /ysm-gpfs/home/jss245/scratch/20161215consensus/jobs.list
-done
-
--- multisplice.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-input=~/documents/school/grad/townsend/ntetncras/input/*
-
-for f in $input;
-do
-	( ./splicer.py -csv $f -gf /Users/jaystanley/Documents/School/Grad/townsend/ntetncras/ncr1_genes.gff -fas /Users/jaystanley/Documents/School/Grad/townsend/ntetncras/ntet_cds.fasta & ); 
-done    
